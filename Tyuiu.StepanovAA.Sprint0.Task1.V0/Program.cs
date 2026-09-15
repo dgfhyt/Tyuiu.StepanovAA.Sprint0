3d5d4a36816e47c157ac1d5cb00efda0b13f7183
dgfhyt <dgfhyt@gmail.com>
2026-09-15 23:40:29 +0500

    Add Task1

---
+++ Tyuiu.StepanovAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.StepanovAA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            String name = "Artem";
+            int age = 17;
+
+            Console.WriteLine("Hello, my name is " + name + " and I'm " + age + " years old!");
+        }
+    }
+}
