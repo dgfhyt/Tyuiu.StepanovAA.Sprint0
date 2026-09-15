3db979e745dedc2f415e5d030f46972091ffa703
stepa <stepa@DESKTOP-G9HV8J7>
2026-09-15 20:56:58 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.StepanovAA.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.StepanovAA.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
