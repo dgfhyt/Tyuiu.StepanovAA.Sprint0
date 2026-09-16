e802b1c9efb415600a390f9d721399e03749ca4f
dgfhyt <dgfhyt@gmail.com>
2026-09-16 10:04:40 +0500

    Add Task 3

---
+++ Tyuiu.StepanovAA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.StepanovAA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.StepanovAA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+        }
+    }
+}
