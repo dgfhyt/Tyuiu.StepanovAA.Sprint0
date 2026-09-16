4da1489fd10f414b5f6580378991da7405dafbdf
dgfhyt <dgfhyt@gmail.com>
2026-09-16 19:24:40 +0500

    Add Task5

---
+++ Tyuiu.StepanovAA.Sprint0.Task5.V0.Lib/DataServise.cs
@@ -0,0 +1,33 @@
+﻿namespace Tyuiu.StepanovAA.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Division by Zero");
+                return -1;
+            }
+            else
+            {
+                return (double)a / b;
+            }
+        }
+    }
+}
---
+++ Tyuiu.StepanovAA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using Tyuiu.StepanovAA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.StepanovAA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"a + b = {DataService.Addition(1, 5)}");
+            Console.WriteLine($"a - b = {DataService.Subtraction(15, 5)}");
+            Console.WriteLine($"a * b = {DataService.Multiplication(10, 10)}");
+            Console.WriteLine($"a / b = {DataService.Division(9, 3)}");
+            Console.WriteLine($"a / b = {DataService.Division(5, 0)}");
+        }
+    }
+}
